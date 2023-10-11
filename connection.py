371725fcbc8d54d4e3748766059357bdb5131b82
nifvimp <kylewmoy@gmail.com>
2023-10-10 20:51:27 -0400

    testing takeoff

--- connection.py
+++ connection.py
@@ -2,30 +2,38 @@ from pymavlink import mavutil
 from constants import GSC_HOST as host
 from constants import GSC_PORT as port
 
-connection = mavutil.mavlink_connection('udpin:%u:%u' % (host, port))
+connection_string = 'udpin:%s:%s' % (host, port)
+connection = mavutil.mavlink_connection(connection_string)
 connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" %
       (connection.target_system, connection.target_component))
 
-# msg = the_connection.recv_match(type='ATTITUDE', blocking=True)
-# print(msg)
+def guided():
+    connection.mav.command_long_send(
+        connection.target_system,
+        connection.target_component,
+        mavutil.mavlink.MAV_CMD_DO_SET_MODE,
+        0, 1, 216, 0, 0, 0, 0, 0)
 
 def arm():
-    connection.mav.command_long_send(connection.target_system,
-                                     connection.target_component,
-                                     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-                                     0, 1, 0, 0, 0, 0, 0, 0)
+    connection.mav.command_long_send(
+        connection.target_system,
+        connection.target_component,
+        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+        0, 1, 0, 0, 0, 0, 0, 0)
 
 
 def disarm():
-    connection.mav.command_long_send(connection.target_system,
-                                     connection.target_component,
-                                     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-                                     0, 0, 0, 0, 0, 0, 0, 0)
+    connection.mav.command_long_send(
+        connection.target_system,
+        connection.target_component,
+        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+        0, 0, 0, 0, 0, 0, 0, 0)
 
 
 def takeoff(altitude):
-    connection.mav.command_long_send(connection.target_system,
-                                     connection.target_component,
-                                     mavutil.mavlink.MAV_CMD_TAKEOFF,
-                                     0, 0, 0, 0, 0, 0, 0, altitude)
+    connection.mav.command_long_send(
+        connection.target_system,
+        connection.target_component,
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+        0, 0, 0, 0, 0, 0, 0, altitude)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import connection
 
 if __name__ == '__main__':
+    connection.guided()
     connection.arm()
     connection.takeoff(20)
