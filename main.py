fb4a9d8b8c0a20f54a5c50e33cfe2bcda804023d
nifvimp <kylewmoy@gmail.com>
2023-10-10 22:30:44 -0400

    fixed bugs

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import connection
 if __name__ == '__main__':
     connection.guided()
     connection.arm()
-    connection.motors_armed_wait()
     connection.takeoff(20)
     time.sleep(10)
     connection.land()
