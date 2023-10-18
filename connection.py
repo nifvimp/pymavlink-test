5e164fb8f99e6391b381c1c3da3d22b3993cd529
nifvimp <kylewmoy@gmail.com>
2023-10-17 20:22:35 -0400

    testing

--- connection.py
+++ connection.py
@@ -1,60 +1,32 @@
-from pymavlink import mavutil
-from constants import GSC_HOST as host
-from constants import GSC_PORT as port
-
-connection_string = 'udpin:%s:%s' % (host, port)
-connection = mavutil.mavlink_connection(connection_string)
-connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" %
-      (connection.target_system, connection.target_component))
-
-def guided():
-    connection.mav.set_mode_send(
-         connection.target_system,
-         mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
-         connection.mode_mapping()['GUIDED'])
-
-
-def stabilize():
-    connection.mav.set_mode_send(
-         connection.target_system,
-         mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
-         connection.mod_mapping()['STALIZE'])
-
-
-def arm():
-    connection.mav.command_long_send(
-        connection.target_system,
-        connection.target_component,
-        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-        0,
-        1, 0, 0, 0, 0, 0, 0)
-    connection.motors_armed_wait()
-
-
-def disarm():
-    connection.mav.command_long_send(
-        connection.target_system,
-        connection.target_component,
-        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-        0,
-        0, 0, 0, 0, 0, 0, 0)
-    connection.motors_disarmed_wait()
-
-
-def takeoff(altitude):
-    connection.mav.command_long_send(
-        connection.target_system,
-        connection.target_component,
-        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
-        0,
-        0, 0, 0, 0, 0, 0, altitude)
-
-
-def land():
-    connection.mav.command_long_send(
-        connection.target_system,
-        connection.target_component,
-        mavutil.mavlink.MAV_CMD_NAV_LAND,
-        0,
-        0, 0, 0, 0, 0, 0, 0)
+# def guided():
+#     connection.mav.set_mode_send(
+#          connection.target_system,
+#          mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+#          connection.mode_mapping()['GUIDED'])
+#
+#
+# def stabilize():
+#     connection.mav.set_mode_send(
+#          connection.target_system,
+#          mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+#          connection.mod_mapping()['STALIZE'])
+#
+#
+# def arm():
+#     connection.mav.command_long_send(
+#         connection.target_system,
+#         connection.target_component,
+#         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+#         0,
+#         1, 0, 0, 0, 0, 0, 0)
+#     connection.motors_armed_wait()
+#
+#
+# def disarm():
+#     connection.mav.command_long_send(
+#         connection.target_system,
+#         connection.target_component,
+#         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+#         0,
+#         0, 0, 0, 0, 0, 0, 0)
+#     connection.motors_disarmed_wait()
--- main.py
+++ main.py
@@ -1,9 +1,38 @@
+from pymavlink import mavutil
 import time
-import connection
+
+GSC_HOST = '172.22.84.123'
+GSC_PORT = 14550
+
+def takeoff(connection, altitude):
+    connection.mav.command_long_send(
+        connection.target_system,
+        connection.target_component,
+        mavutil.mavlink.MAV_CMD_NAV_TAKEOFF,
+        0,
+        0, 0, 0, 0, 0, 0, altitude)
+
+
+def land(connection):
+    connection.mav.command_long_send(
+        connection.target_system,
+        connection.target_component,
+        mavutil.mavlink.MAV_CMD_NAV_LAND,
+        0,
+        0, 0, 0, 0, 0, 0, 0)
 
 if __name__ == '__main__':
-    connection.guided()
-    connection.arm()
-    connection.takeoff(20)
+    connection_string = 'udpin:%s:%s' % (GSC_HOST, GSC_PORT)
+    connection = mavutil.mavlink_connection(connection_string)
+    connection.wait_heartbeat()
+    print("Heartbeat from system (system %u component %u)" %
+          (connection.target_system, connection.target_component))
+
+    connection.set_mode(mode=connection.mode_mapping()['GUIDED'])
+    connection.arducopter_arm()
+    land()
+
+    takeoff(connection, 20)
     time.sleep(10)
-    connection.land()
+    land(connection)
+
